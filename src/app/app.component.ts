11045dfe9ff65d3f906cc7a4869e34d19531fb9c
Sharan777 <sharangouda021@gmail.com>
2021-01-07 17:19:54 +0530

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { ResizedEvent } from 'angular-resize-event';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,20 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  width: number;
+  height: number;
+ 
+  onResized(event: ResizedEvent) {
+    this.width = event.newWidth;
+    this.height = event.newHeight;
+    console.log('width', this.width);
+    console.log('height', this.height);
+  }
+
+  groups=[1,2,3,4,5,6,7,8,9,10,11]
+  
+  onDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.groups, event.previousIndex, event.currentIndex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AngularResizedEventModule } from 'angular-resize-event';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AngularResizedEventModule, DragDropModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
